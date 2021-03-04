3b234ad89a759de10244e00c319384221efbc41d
fstar-mac <hhbuhuiai@163.com>
2021-03-04 10:45:13 +0800

    add 77. Combinations

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ My Leetcode: https://leetcode.com/fstars/
 - `2021.02.28` [228. Summary Ranges](src/Array/228.summary-ranges[easy]/index.ts) `easy` `has better solved`
 - `2021.03.01` [39. Combination Sum](src/Array/39.combination-sum[medium]/index.ts) `medium`
 - `2021.03.02` [40. Combination Sum II](src/Backtracking/40.combination-sum-ii[medium]/index.ts) `medium`
+- `2021.03.03` 77. Combinations
--- src/Backtracking/77.combinations/index.ts
+++ src/Backtracking/77.combinations/index.ts
@@ -2,5 +2,19 @@
  * src: https://leetcode.com/problems/combinations/
  */
 function combine(n: number, k: number): number[][] {
-  // 
+  const combs: number[][] = []
+  function dfs(remain: number, comb: number[], cur: number) {
+    if (n - cur + 1 < remain) { // 剪枝
+      return
+    }
+    if (remain === 0) {
+      combs.push(comb)
+      return
+    }
+
+    dfs(remain, comb, cur + 1) // 跳过
+    dfs(remain - 1, [...comb, cur], cur + 1) // 选当前值
+  }
+  dfs(k, [], 1)
+  return combs
 };
